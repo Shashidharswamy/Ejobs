c58a2ead25d136843096497aff5c0efc85ecf70f
manju <manju@DESKTOP-C926226>
2022-08-27 17:19:23 +0530

    updated

--- src/main/java/searchJob/Search_JobBy_Industry_Name.java
+++ src/main/java/searchJob/Search_JobBy_Industry_Name.java
@@ -3,5 +3,6 @@ package searchJob;
 public class Search_JobBy_Industry_Name {
 public static void main(String[] args) {
 	System.out.println("150 jobs available");
+	System.out.println("no need job hogolo  ");
 }
 }
