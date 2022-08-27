b656682d2ded1b10e77d7ddaa84a4259cddbc39b
Toshiba <Toshiba@DESKTOP-CSSJGEK>
2022-08-27 18:22:32 +0530

    check it

--- src/main/java/searchJob/Search_JobBy_Industry_Name.java
+++ src/main/java/searchJob/Search_JobBy_Industry_Name.java
@@ -3,5 +3,8 @@ package searchJob;
 public class Search_JobBy_Industry_Name {
 public static void main(String[] args) {
 	System.out.println("150 jobs available");
+	System.out.println("ok fine");
+	System.out.println("not at getting");
+	
 }
 }
