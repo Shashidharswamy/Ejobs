e26647d86b6bc832b3cb25346f4ae2e0a3274305
Shashidharswamy <111877606+Shashidharswamy@users.noreply.github.com>
2022-08-29 15:04:57 +0530

    Update Search_JobBy_Industry_Name.java

--- src/main/java/searchJob/Search_JobBy_Industry_Name.java
+++ src/main/java/searchJob/Search_JobBy_Industry_Name.java
@@ -2,7 +2,7 @@ package searchJob;
 
 public class Search_JobBy_Industry_Name {
 public static void main(String[] args) {
-	System.out.println("140 jobs available");
+	System.out.println("130 jobs available");
 
 	
 }
